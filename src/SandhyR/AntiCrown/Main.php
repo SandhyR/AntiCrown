c72cad5d8dfa0089aa66a4ea4735f9cfb9effb3c
SandhyR <sandhyraihan36@gmail.com>
2022-02-21 12:57:47 +0700

    Fix

--- src/SandhyR/AntiCrown/Main.php
+++ src/SandhyR/AntiCrown/Main.php
@@ -3,7 +3,7 @@
 namespace SandhyR\AntiCrown;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerPreLoginEvent;
+use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\plugin\PluginBase;
 use VenityNetwork\CurlLib\CurlLib;
 use VenityNetwork\CurlLib\CurlResponse;
@@ -18,15 +18,15 @@ class Main extends PluginBase implements Listener{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-    public function onPreLogin(PlayerPreLoginEvent $event){
+    public function onLogin(PlayerLoginEvent $event){
         $url = "https://player.venitymc.com/api/v1/guild/crown";
         $headers = [];
         $curlOpts = [];
         $curl = CurlLib::init($this, 1);
         $curl->get($url, $headers, $curlOpts, function(CurlResponse $response) use ($event) {
             $result = json_decode($response->getBody(), true);
-            if(in_array(strtolower($event->getPlayerInfo()->getUsername()), array_merge($result["officers"], $result["members"], [$result["leader"]]))){
-                $event->setKickReason(PlayerPreLoginEvent::KICK_REASON_PLUGIN, "Kron");
+            if(in_array(strtolower($event->getPlayer()->getName()), array_merge($result["officers"], $result["members"], [$result["leader"]]))){
+                $event->getPlayer()->kick("Kron");
             }
         });
     }
