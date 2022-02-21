592520037279efb14264768c744bcfe7dfc25d39
SandhyR <sandhyraihan36@gmail.com>
2022-02-21 12:25:40 +0700

    Use mamal lib

--- src/SandhyR/AntiCrown/Main.php
+++ src/SandhyR/AntiCrown/Main.php
@@ -3,32 +3,31 @@
 namespace SandhyR\AntiCrown;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerLoginEvent;
-use pocketmine\player\Player;
+use pocketmine\event\player\PlayerPreLoginEvent;
 use pocketmine\plugin\PluginBase;
+use VenityNetwork\CurlLib\CurlLib;
+use VenityNetwork\CurlLib\CurlResponse;
 
 class Main extends PluginBase implements Listener{
 
-    private static $instance;
-
     public function onEnable(): void
     {
-        self::$instance = $this;
+        if(!class_exists(CurlLib::class)){
+            throw new \ErrorException("Install CurlLib from https://github.com/VenityNetwork/CurlLib");
+        }
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
     }
 
-    public function onLogin(PlayerLoginEvent $event){
-        $this->getServer()->getAsyncPool()->submitTask(new QueryAsyncTask($event->getPlayer()->getName()));
+    public function onPreLogin(PlayerPreLoginEvent $event){
+        $url = "https://player.venitymc.com/api/v1/guild/crown";
+        $headers = [];
+        $curlOpts = [];
+        $curl = CurlLib::init($this, 1);
+        $curl->get($url, $headers, $curlOpts, function(CurlResponse $response) use ($event) {
+            $result = json_decode($response->getBody(), true);
+            if(in_array(strtolower($event->getPlayerInfo()->getUsername()), array_merge($result["officers"], $result["members"], [$result["leader"]]))){
+                $event->setKickReason(PlayerPreLoginEvent::KICK_REASON_PLUGIN, "Kron");
+            }
+        });
     }
-
-    public function kickPlayer(string $username){
-        $player = $this->getServer()->getPlayerExact($username);
-        if($player instanceof Player){
-            $player->kick("Kron");
-        }
-    }
-
-    public static function getInstance(): self{
-        return self::$instance;
-}
 }
